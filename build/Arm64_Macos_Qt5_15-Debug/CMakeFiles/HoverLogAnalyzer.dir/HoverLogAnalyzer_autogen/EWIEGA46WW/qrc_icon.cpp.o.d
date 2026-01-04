13bceade733aa01283993d0dd1e9ea102ab35e23
dzs <2863238200@qq.com>
2026-01-04 10:42:21 +0800

    remove build directory and .DS_Store from tracking

--- build/Arm64_Macos_Qt5_15-Debug/CMakeFiles/HoverLogAnalyzer.dir/HoverLogAnalyzer_autogen/EWIEGA46WW/qrc_icon.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/HoverLogAnalyzer.dir/HoverLogAnalyzer_autogen/EWIEGA46WW/qrc_icon.cpp.o: \
-  /Users/dzs/Downloads/ZZTools/HoverLogAnalyzer/build/Arm64_Macos_Qt5_15-Debug/HoverLogAnalyzer_autogen/EWIEGA46WW/qrc_icon.cpp
